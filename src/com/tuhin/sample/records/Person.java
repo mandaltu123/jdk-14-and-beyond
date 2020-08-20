f1f43884ed94d2fdbff6b4af7163eb1698c068e6
mandaltu123 <myid.tuhin123@gmail.com>
2020-08-20 10:24:36 +0530

    adding static methods and constructor to records

--- src/com/tuhin/sample/records/Person.java
+++ src/com/tuhin/sample/records/Person.java
@@ -1,6 +1,32 @@
 package com.tuhin.sample.records;
 
 record Person(String firstName, String lastName) {
+
+    static int x;
+
+//    We can also add constructors, and modify the canonical constructor
+//    (the one that takes the two String parameters). If we want to override
+//    the canonical constructor, we can omit the parameters and the assignments
+//    to the fields:
+
+    public Person {
+        if (firstName == null && lastName == null) {
+            try {
+                throw new IllegalAccessException("Firstname and lastname can not be null");
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    //We can define static methods and instance methods that can operate on the state of the object:
+    public static void doX() {
+        x++;
+    }
+
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
 }
 
 class Test {
